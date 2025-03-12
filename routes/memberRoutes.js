364caa6ce0bc1d4b415007ec2521617c16a120f6
Shiva0909122 <shivarth.drona@gmail.com>
2025-03-12 08:14:55 +0530

    Fix CORS for live frontend

--- routes/memberRoutes.js
+++ routes/memberRoutes.js
@@ -1,10 +1,38 @@
+// const express = require("express");
+// const { getMembers, addMember, deleteMember } = require("../controllers/memberController");
+
+// const router = express.Router();
+
+// router.get("/members", getMembers);
+// router.post("/members", addMember);
+// router.delete("/members/:id", deleteMember);
+
+// module.exports = router;
+
 const express = require("express");
-const { getMembers, addMember, deleteMember } = require("../controllers/memberController");
-
 const router = express.Router();
+const Member = require("../models/Member");  // Make sure the model is correctly imported
 
-router.get("/members", getMembers);
-router.post("/members", addMember);
-router.delete("/members/:id", deleteMember);
+// GET all members
+router.get("/", async (req, res) => {
+    try {
+        const members = await Member.find();
+        res.json(members);
+    } catch (err) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+// POST new member
+router.post("/", async (req, res) => {
+    const { name, parent } = req.body;
+    try {
+        const newMember = new Member({ name, parent });
+        await newMember.save();
+        res.json(newMember);
+    } catch (err) {
+        res.status(500).json({ error: "Server error" });
+    }
+});
 
 module.exports = router;
