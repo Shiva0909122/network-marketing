0f3f0129be6ddcc120b9ca86f62c4b90aeb82190
Shiva0909122 <shivarth.drona@gmail.com>
2025-03-12 08:09:13 +0530

    fixed

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(cors({ origin: "*" })); // Allow all domains (for testing)
 
 app.use(express.json());
 
-// Connect to MongoDB
+// Connect to MongoDB done
 connectDB();
 
 // Routes
 app.use("/api/members", require("./routes/memberRoutes"));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`)); 
